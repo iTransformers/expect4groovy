ca30d50d8f8a62b01b8490d7bc46696eeb4c6ff0
vasko <vasko@vasko-home.(none)>
2014-03-30 13:22:45 +0300

    Added timeout and eof test cases

---
+++ src/test/java/net/itransformers/expect4groovy/test_timeout.groovy
@@ -0,0 +1,15 @@
+package net.itransformers.expect4groovy
+
+boolean status = false
+send "hello\n"
+expect([
+        _gl("hello\n") {
+            println "Hello World!"
+            Thread.sleep(1100)
+            it.exp_continue()
+        },
+        _timeout(1000){
+            status = true
+        }
+])
+return status
